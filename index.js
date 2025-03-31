c9f8ae933d83ce4e8ec633f72b287788800b0cc6
Nic Ciccone <nicciccone1205@gmail.com>
2025-03-31 19:00:06 -0400

    second commit

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-strComplete = `Please enter a comma-separated list of Froyo flavors.`
+let flavors = prompt("Please enter a list of comma-separated Froyo flavors.");
 
-alert(strComplete)
+flavors = flavors.split(".");
+
+const countFlavor = (arr) => {
+    const flavCount = {};
+    for (let flavor of arr) {
+        flavor = flavor.trim();
+        flavor in flavCount ? (flavCount[flavor] += 1) : (flavCount[flavor] = 1);
+    }
+    return flavCount
+};
+
+console.table(countFlavor(flavors));
